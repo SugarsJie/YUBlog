93cc48653996434177c939447ced52b660c12db5
york9527 <villagearcher@gmail.com>
2016-05-07 22:50:26 +0800

    add using of mongodb

---
+++ YuBlog/WebUI/models/Blog.js
@@ -0,0 +1,17 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Blog = new Schema({
+    title: String,
+    author: String,
+    body: String,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+        votes: Number,
+        favs: Number
+    }
+});
+
+module.exports = mongoose.model('Blog', Blog);
--- YuBlog/WebUI/routes/manage.js
+++ YuBlog/WebUI/routes/manage.js
@@ -1,5 +1,6 @@
 ﻿var express = require('express');
 var passport = require('passport');
+var Blog = require('../models/blog');
 var router = express.Router();
 
 function loggedIn(req, res, next) {
@@ -12,6 +13,19 @@ function loggedIn(req, res, next) {
 
 router.get('/', loggedIn, function (req, res, next) {
     res.send('wellcom' + req.user.username);
+});
+
+router.get('/create', loggedIn, function (req, res, next) {
+    var blog = new Blog({
+        title: 'test blog',
+        author: '余正文',
+        body: '123456789abcdefg'
+    });
+
+    blog.save(function (err) { 
+        
+    });
+
 });
 
 module.exports = router;
