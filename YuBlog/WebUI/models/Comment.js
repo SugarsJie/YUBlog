0b66d341a6a137109e1bbde5cccfc8fe159003d4
Administrator <admin@example.com>
2017-01-24 14:24:49 +0800

    add comment model

---
+++ YuBlog/WebUI/models/Comment.js
@@ -0,0 +1,14 @@
+﻿var mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate');
+
+var Schema = mongoose.Schema;
+
+var Comment = new Schema({
+    blogId: ObjectId,
+    replyToId: ObjectId,
+    userName: String,
+    content: String,
+    date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Comment', Comment);
